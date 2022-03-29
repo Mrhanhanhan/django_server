90a81b82905dfd4090ada52d185673dba32992bb
han <helvishan@gmail.com>
2022-03-29 17:29:52 +0800

    adding background image

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -7,11 +7,4 @@ class AcGameMenu {
         `);
         this.root.$ac_game.append(this.$menu);
     }
-}class AcGame {
-    constructor(id) {//构造函数
-        this.id = id;
-        this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
-        console.log("create acgame")
-    }
 }
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,6 +1,8 @@
 class AcGame {
     constructor(id) {//构造函数
-
-
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        console.log("create acgame")
     }
 }
