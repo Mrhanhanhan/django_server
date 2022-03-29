7a645107c3da95a53fa0821dcb2d2db89f05c603
han <helvishan@gmail.com>
2022-03-29 17:40:44 +0800

    debug

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -3,11 +3,14 @@ class AcGameMenu {
         this.root = root;
         this.$menu = $(`
             <div class="ac-game-menu">
+            <p>你好</p>
             </div>
         `);
         this.root.$ac_game.append(this.$menu);
     }
-}class AcGame {
+}
+
+class AcGame {
     constructor(id) {//构造函数
         this.id = id;
         this.$ac_game = $('#' + id);
