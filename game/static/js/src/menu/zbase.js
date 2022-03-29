3b58663ac09d59b0d2cd4ffe9821026bbde2a75c
han <helvishan@gmail.com>
2022-03-29 17:49:05 +0800

    debug

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -3,7 +3,7 @@ class AcGameMenu {
         this.root = root;
         this.$menu = $(`
             <div class="ac-game-menu">
-            <p>你好</p>
+           
             </div>
         `);
         this.root.$ac_game.append(this.$menu);
